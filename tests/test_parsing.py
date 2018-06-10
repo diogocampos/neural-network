fddf0ff935c0a4e18b816de9db80843abcd011f9
Diogo Campos <diogocampos@users.noreply.github.com>
2018-06-10 14:24:42 -0300

    Add leitura do arquivo dataset.txt

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -9,3 +9,8 @@ def test_parse_weights_file(example):
     filename = example['files']['weights']
     weights = parsing.parse_weights_file(filename)
     assert weights == example['weights']
+
+def test_parse_dataset_file(example):
+    filename = example['files']['dataset']
+    dataset = parsing.parse_dataset_file(filename)
+    assert dataset == example['dataset']
--- trabalho2/parsing.py
+++ trabalho2/parsing.py
@@ -19,3 +19,16 @@ def parse_weights_file(filename):
         layer = [[float(weight) for weight in neuron] for neuron in neurons]
         layers.append(layer)
     return layers
+
+
+def parse_dataset_file(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    instances = []
+    for line in lines:
+        features, outputs = [values.split(',') for values in line.split(';')]
+        features = [float(value) for value in features]
+        outputs = [float(value) for value in outputs]
+        instances.append([features, outputs])
+    return instances
