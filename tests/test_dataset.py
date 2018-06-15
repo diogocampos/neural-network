ee97a641026fb01d904d02932b300efe01341968
Diogo Campos <diogocampos@users.noreply.github.com>
2018-06-15 20:37:20 -0300

    Add normalização dos features do dataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -11,3 +11,19 @@ def test_expectations(example):
     instances = example['dataset']
     dataset = Dataset(instances)
     assert dataset.expectations.tolist() == [i[1] for i in instances]
+
+
+def test_normalize():
+    features = [
+        [0.0, -1.0],
+        [1.0,  1.0],
+        [4.0,  0.0],
+    ]
+    normalized_features = [
+        [0.00, 0.0],
+        [0.25, 1.0],
+        [1.00, 0.5],
+    ]
+    instances = [(xs, [0.0]) for xs in features]
+    dataset = Dataset(instances, normalize=True)
+    assert dataset.features.tolist() == normalized_features
--- trabalho2/dataset.py
+++ trabalho2/dataset.py
@@ -3,10 +3,15 @@ import numpy as np
 
 class Dataset:
 
-    def __init__(self, instances):
+    def __init__(self, instances, normalize=False):
         # Inicializa um dataset.
         # - instances: lista de instâncias de treinamento
         # (cada instância é uma tupla de duas listas: atributos e saídas)
 
         self.features = np.array([i[0] for i in instances])
         self.expectations = np.array([i[1] for i in instances])
+
+        if normalize:
+            maximums = np.max(self.features, axis=0)
+            minimums = np.min(self.features, axis=0)
+            self.features = (self.features - minimums) / (maximums - minimums)
