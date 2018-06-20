87f277d083b720f2214e908840d2e7e07ac65852
Diogo Campos <diogocampos@users.noreply.github.com>
2018-06-20 14:35:38 -0300

    Add script de classificação dos resultados

---
+++ results/sort_results.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import json
+from pathlib import Path
+import sys
+
+
+def main(argv):
+
+    if len(argv) < 2:
+        print('Usage: %s RESULTS_DIR' % argv[0], file=sys.stderr)
+        return 1
+
+    dirname = argv[1]
+    results = load_results(dirname)
+    sorted_results = sorted(results, key=lambda r: get_average(r, 'j_cv'))
+    print_results(sorted_results)
+
+
+def load_results(dirname):
+
+    files = Path(dirname).glob('*.json')
+    results = []
+
+    for filename in files:
+        with open(filename, 'r') as file:
+            data = json.loads(file.read())
+            results.append(data)
+    
+    return results
+
+
+def get_average(result, key):
+    # Calcula o valor médio de fold[key] para um arquivo de resultados.
+
+    values = [fold[key] for fold in result['folds']]
+    average = sum(values) / len(values)
+    return average
+
+
+def print_results(results):
+
+    table = [['Structure', 'Lambda', 'Average J_cv', 'Average F1_score']]
+
+    for result in results:
+        row = [
+            result['structure'],
+            result['lambda'],
+            get_average(result, 'j_cv'),
+            get_average(result, 'f1_score'),
+        ]
+        table.append(row)
+
+    for row in table:
+        print('%-20s %-20s %-20s %-20s' % tuple(row))
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
