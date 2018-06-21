08bd13847a8adb2f14db5deca91febf1a5eb90cf
Diogo Campos <diogocampos@users.noreply.github.com>
2018-06-20 21:52:24 -0300

    Incluir desvios padrão nos resultados tabulados

--- results/sort_results.py
+++ results/sort_results.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import numpy as np
+
 import json
 from pathlib import Path
 import sys
@@ -15,7 +17,8 @@ def main(argv):
     use_csv = (len(argv) > 2 and argv[2] == '--csv')
 
     results = load_results(dirname)
-    sorted_results = sorted(results, key=lambda r: get_average(r, 'j_cv'))
+    average_jcv = lambda result: np.mean(get_values(result, 'j_cv'))
+    sorted_results = sorted(results, key=average_jcv)
 
     print_results(sorted_results, use_csv)
 
@@ -33,20 +36,12 @@ def load_results(dirname):
     return results
 
 
-def get_average(result, key):
-    # Calcula o valor médio de fold[key] para um arquivo de resultados.
-
-    values = [fold[key] for fold in result['folds']]
-    average = sum(values) / len(values)
-    return average
-
-
 def print_results(results, use_csv=False):
 
     if use_csv:
-        row_format = '"%s",%s,%s,%s,%s'
+        row_format = '"%s",%s,%s,%s,%s,%s,%s,%s'
     else:
-        row_format = '%-12s %-10s %-20s %-20s %-20s'
+        row_format = '%-12s %-10s %-22s %-22s %-22s %-22s %-22s %-22s'
 
     table = format_results(results)
     for row in table:
@@ -55,19 +50,36 @@ def print_results(results, use_csv=False):
 
 def format_results(results):
 
-    table = [['Structure', 'Lambda', 'Avg J_t', 'Avg J_cv', 'Avg F1_score']]
+    table = [[
+        'Structure',
+        'Lambda',
+        'Avg J_t',
+        'Stddev J_t',
+        'Avg J_cv',
+        'Stddev J_cv',
+        'Avg F1_score',
+        'Stddev F1_score'
+    ]]
 
     for result in results:
         row = [
             ' '.join(str(val) for val in result['structure']),
             result['lambda'],
-            get_average(result, 'j_t'),
-            get_average(result, 'j_cv'),
-            get_average(result, 'f1_score'),
+            np.mean(get_values(result, 'j_t')),
+            np.std(get_values(result, 'j_t')),
+            np.mean(get_values(result, 'j_cv')),
+            np.std(get_values(result, 'j_cv')),
+            np.mean(get_values(result, 'f1_score')),
+            np.std(get_values(result, 'f1_score')),
         ]
         table.append(row)
 
     return table
+
+
+def get_values(result, key):
+    values = [fold[key] for fold in result['folds']]
+    return np.array(values)
 
 
 if __name__ == '__main__':
